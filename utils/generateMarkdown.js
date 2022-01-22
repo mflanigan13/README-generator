b9b63c27678c61186dfc80e69bb563a7763fc78c
mflanigan <mflanigantwualumn@gmail.com>
2022-01-22 12:20:26 -0600

    added license functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,50 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'no license') {
+    return `
+  ![badge](https://img.shields.io/badge/license-${license}-blue)
+    `;
+  } else {
+    return ' ';
+  }
+};
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function renderLicenseLink(license) {
+  if (license !== 'no license') {
+    return `
+    [${license}](https://choosealicense.com/licenses/${license})
+      `;
+  } else {
+    return ' ';
+  }
 }
 
-module.exports = generateMarkdown;
+// Function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  function renderLicenseSection(license) {
+    if (license !== 'no license') {
+      return `
+    ## [License](#table-of-contents)
+    The application is covered under the following license:
+    ${renderLicenseLink(license)}
+      `;
+    } else {
+      return ' ';
+    }
+  };
+
+  // Function to generate markdown for README
+  function generateMarkdown(data) {
+    return `# ${data.title}
+  
+  function generateMarkdown(data) {
+    return `
+    # ${ data.title }
+
+  }
+
+  module.exports = generateMarkdown;
