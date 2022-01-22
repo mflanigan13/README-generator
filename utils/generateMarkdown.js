95289bccabeb3f6e8ecd5605bb92eeb2f79fa305
mflanigan <mflanigantwualumn@gmail.com>
2022-01-22 12:37:22 -0600

    wrote generateMarkdown function to generate markdown for README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -25,7 +25,6 @@ function renderLicenseLink(license) {
 // Function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  function renderLicenseSection(license) {
     if (license !== 'no license') {
       return `
     ## [License](#table-of-contents)
@@ -39,12 +38,43 @@ function renderLicenseSection(license) {
 
   // Function to generate markdown for README
   function generateMarkdown(data) {
-    return `# ${data.title}
-  
-  function generateMarkdown(data) {
     return `
-    # ${ data.title }
-
+    # ${data.title}
+  
+    ${renderLicenseBadge(data.license)}
+    ## Table-of-Contents
+    * [Description](#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    ${renderLicenseTOC(data.license)}
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    
+    ## [Description](#table-of-contents)
+    ${data.what}
+    ${data.why}
+    ${data.how}
+    ## [Installation](#table-of-contents)
+    ${data.installation}
+    ## [Usage](#table-of-contents)
+    ${data.usage}
+    
+    For more information on how to add screenshots for examples, visit the following website:
+    
+    [Mark Down Tutorial](https://agea.github.io/tutorial.md/)
+    
+    ${renderLicenseSection(data.license)}
+    ## [Contributing](#table-of-contents)
+    
+    ${renderContributingSection(data.confirmContributers, data.contribute)}
+    ## [Tests](#table-of-contents)
+    ${data.test}
+    ## [Questions](#table-of-contents)
+    Please contact me using the following links:
+    [GitHub](https://github.com/${data.githubUsername})
+    [Email: ${data.email}](mailto:${data.email})
+  `;
   }
 
   module.exports = generateMarkdown;
